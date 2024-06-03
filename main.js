0e85796feaeb0a9a6eea1337c0c6d2ceec2bfd78
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-06-03 20:16:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// SEGUNDO EJERCICIO /  Palio, rojo
+//****Entrada****//
+let auto = prompt('Describa la marca de su auto')
+let color = prompt('Ingrese el color')
+//****Proceso****//
+let mensaje = `Hola mi vehiculo es un ${auto}, de color ${color}`
+//****Salida****// 
+console.log(mensaje)
+console.log(mensaje.length)
